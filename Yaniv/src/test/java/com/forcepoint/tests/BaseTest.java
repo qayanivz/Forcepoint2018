698dcecbd64123d3083ce21a1de396553c102665
יניב זואי <yaniv-z@yedioth.co.il>
2018-11-11 18:50:10 +0200

    Yaniv November18

--- Yaniv/src/test/java/com/forcepoint/tests/BaseTest.java
+++ Yaniv/src/test/java/com/forcepoint/tests/BaseTest.java
@@ -17,42 +17,38 @@ import com.forcepoint.pages.BasePage;
 import com.forcepoint.utilities.ExtentManager;
 import com.forcepoint.utilities.Utilities;
 
-
 public class BaseTest {
 
 	public WebDriver driver;
 	public ExtentTest test;
 	public ExtentReports report;
-	
 
 	@BeforeClass
 	public WebDriver init() throws IOException, InterruptedException {
 
 		driver = Utilities.getBrowser();
-//		extent.log(Status.INFO, "Browser Started");
+
 		driver.manage().window().maximize();
-//		extent.log(Status.INFO, "Browser Maximize");
+
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(Utilities.getUrl());
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--enable-extensions");
 		options.addArguments("--disable-infobars");
-		
 
 		return driver;
 	}
 
 	@BeforeTest
-	public void setUp()
-	{
+	public void setUp() {
 		report = ExtentManager.getInstance();
-		test = report.createTest("Yanivvvv num2");
-		
+//		test = report.createTest("Yanivvvv num2");
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
+		test.log(Status.INFO, "Browser Closed");
 		report.flush();
 	}
 
--- Yaniv/src/test/java/com/forcepoint/tests/SortTest.java
+++ Yaniv/src/test/java/com/forcepoint/tests/SortTest.java
@@ -4,15 +4,11 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 import com.forcepoint.pages.MainPage;
 
-
-
 public class SortTest extends BaseTest{
-	/**
-	 * Rigorous Test :-)
-	 */
 	@Test
-	public void popularResultsIsSortTest() {
+	public void popularResultsIsSortedTest() {
 		
+		test = report.createTest("popularResultsIsSortedTest");
 		MainPage mainPage = new MainPage(driver, test);
 		mainPage.searchItem("Selenium");
 		mainPage.selectFilterByPopular();
